ce48a85c4d49bc2ca9e68d76c9fd177280524ef9
Zalo Austine <zalo@devs.mobi>
2020-10-05 17:14:39 +0300

    Intent to Info Activity Implementation

--- app/src/main/java/com/pluralsight/candycoded/MainActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/MainActivity.java
@@ -9,6 +9,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
@@ -95,5 +96,12 @@ public class MainActivity extends AppCompatActivity {
 
             db.insert(CandyEntry.TABLE_NAME, null, values);
         }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        Intent infoIntent =  new Intent(MainActivity.this,InfoActivity.class);
+        startActivity(infoIntent);
+        return super.onOptionsItemSelected(item);
     }
 }
